28c640379936e03c11e073f62cc80d90a04c55ff
github username <chrisjbenedict@gmail.com>
2019-04-30 11:01:36 -0400

    posts rendering from jsonplaceholder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,15 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer'
 
 export default combineReducers({
-  replaceMe: () => 'hi there'
+  posts: postsReducer
 });
+
+
+// Rules of reducers:
+  // must return any other value besides 'undefined'
+  // produces 'state', or data to be used inside of your app using only previous state and the action
+  // must not reach 'out of itself' to decide whtat value to return (ex: make an API request)
+    // only looks at previous state value and action payload
+    // reducers are pure
+  // must not mutate its input 'state' arguments
